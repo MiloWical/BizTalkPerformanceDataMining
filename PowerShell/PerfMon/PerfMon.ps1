cd4ce80c1f5ec5a917e9e9c660473e84c10119ba
Milo Wical <Milo.Wical@compare.com>
2018-08-07 14:46:05 -0400

    PoCed the PerfMon reader.

---
+++ PowerShell/PerfMon/PerfMon.ps1
@@ -0,0 +1,90 @@
+
+$Config = Get-Content ".\PerfMon.json" | ConvertFrom-Json
+
+$Tabs = 0;
+
+function GetTabString
+{
+    $retVal = "";
+
+    for ($i = 0; $i -lt $Tabs; $i++)
+    {
+        $retVal += "`t";
+    }
+
+    return $retVal
+}
+
+function WriteOutput($data)
+{
+    Write-Host "$(GetTabString) ${data}";
+}
+
+ForEach($HostEntry in $Config.Hosts) 
+{
+    WriteOutput("Host: $($HostEntry)");
+
+    $Tabs++;
+
+    ForEach($View in $Config.Views)
+    {
+        $ViewName = $View.Name;
+        WriteOutput("View: $ViewName");
+
+        WriteOutput("Counters:");
+
+        $Tabs++;
+
+        ForEach($Counter in $View.Counters)
+        {
+            $CounterName = $Counter.Category + "\" + $Counter.Counter;
+
+            WriteOutput($CounterName);
+
+            $Tabs++
+            
+                If($Counter.Instances -eq $null)
+                {
+                    $Instances = @([System.String]::Empty);        
+                }
+                Else 
+                {
+                    $Instances = $Counter.Instances    
+                }
+
+                ForEach($Instance in $Instances)
+                {
+                    $PerfMonCounter = New-Object System.Diagnostics.PerformanceCounter -ArgumentList $Counter.Category, $Counter.Counter, $Instance, $HostEntry;
+                    $PerfMonValue = $PerfMonCounter.NextSample().RawValue;
+
+
+                    If([System.String]::IsNullOrEmpty($Instance))
+                    {
+                        WriteOutput("Value: ${PerfMonValue}");
+                    }
+                    Else
+                    {
+                        WriteOutput("${Instance}: ${PerfMonValue}");
+                    }
+                }
+
+            $Tabs--;
+        }
+
+        $Tabs--;
+    }
+
+    $Tabs--;
+
+    Write-Host "";
+}
+
+
+
+
+
+<# 
+$Counter = New-Object System.Diagnostics.PerformanceCounter -ArgumentList "Process", "% Processor Time", "_Total", "USECVUT-BTK01";
+
+Write-Host $Counter.NextSample().RawValue;
+#>

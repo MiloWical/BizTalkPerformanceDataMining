d9c6041e2d157e4e95ad3f11f09051427c8f8894
Milo Wical <Milo.Wical@compare.com>
2018-08-08 12:48:36 -0400

    Incremental updates.

--- PowerShell/WMI/WMI.ps1
+++ PowerShell/WMI/WMI.ps1
@@ -8,14 +8,14 @@ function LoadHostWmiProviders($HostName)
         $BizTalkGroup = Get-WmiObject MSBTS_GroupSetting -namespace root\MicrosoftBizTalkServer -ErrorAction Stop -ComputerName $HostName
         $BizTalkMsgBoxDb = Get-WmiObject MSBTS_MsgBoxSetting -namespace root\MicrosoftBizTalkServer -ErrorAction Stop -ComputerName $HostName
         $BizTalkServer = Get-WmiObject MSBTS_Server -namespace root\MicrosoftBizTalkServer -ErrorAction Stop -ComputerName $HostName
-        
+               
         #$registry = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $HostName).OpenSubKey("SOFTWARE\Microsoft\BizTalk Server\3.0")
         #$BizTalkREG = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $HostName).OpenSubKey("SOFTWARE\Microsoft\BizTalk Server\3.0") | Get-ItemProperty -ErrorAction Stop
-        #$BizTalkREG = Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\BizTalk Server\3.0' -ErrorAction Stop
+        $BizTalkREG = Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\BizTalk Server\3.0' -ErrorAction Stop
         
         $HostInstances = Get-WmiObject MSBTS_HostInstance -namespace root\MicrosoftBizTalkServer -ErrorAction Stop -ComputerName $HostName
-        $TrackingHost = Get-WmiObject MSBTS_Host -Namespace root\MicrosoftBizTalkServer -ErrorAction Stop -ComputerName $HostName | Where-Object {$_.HostTracking -eq "true" }
-        
+        $TrackingHost = Get-WmiObject MSBTS_Host -Namespace root\MicrosoftBizTalkServer -ErrorAction Stop -ComputerName $HostName | Where-Object {$_.HostTracking -eq "true" }        
+
         Add-Type -Path ".\Microsoft.RuleEngine.dll"
         Add-Type -Path ".\Microsoft.Biztalk.RuleEngineExtensions.dll"
         Add-Type -Path ".\Microsoft.BizTalk.ExplorerOM.dll"
@@ -23,8 +23,8 @@ function LoadHostWmiProviders($HostName)
         $BizTalkDBInstance = $BizTalkGroup.MgmtDbServerName
         $BizTalkDB = $BizTalkGroup.MgmtDbName
 
-        #$BizTalkOM = New-Object Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer
-        #$BizTalkOM.ConnectionString = "SERVER=$BizTalkDBInstance;DATABASE=$BizTalkDB;Integrated Security=SSPI"
+        $BizTalkOM = New-Object Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer
+        $BizTalkOM.ConnectionString = "SERVER=$BizTalkDBInstance;DATABASE=$BizTalkDB;Integrated Security=SSPI"
         
         $Products = Get-WmiObject win32_product -ErrorAction Stop -ComputerName $HostName
 
@@ -53,13 +53,11 @@ function LoadHostWmiProviders($HostName)
 
 function ServerDetails ($Wmi)
 {
-    Write-Host "Server Details: " -fore Blue
-
-    Write-Host "`nBizTalk Information" -fore Green
-    #Write-Host $BiztalkREG.ProductName "("$BiztalkREG.ProductEdition"Edition )"
-    #Write-Host "Product Version:" $BiztalkREG.ProductVersion
-    #Write-Host "Installation Path:" $BiztalkREG.InstallPath
-    #Write-Host "Installation Date:" $BiztalkREG.InstallDate
+    Write-Host "`nBizTalk Information" -fore DarkGray
+    Write-Host $Wmi.BizTalkREG.ProductName "("$Wmi.BizTalkREG.ProductEdition"Edition )"
+    Write-Host "Product Version:" $Wmi.BizTalkREG.ProductVersion
+    Write-Host "Installation Path:" $Wmi.BizTalkREG.InstallPath
+    Write-Host "Installation Date:" $Wmi.BizTalkREG.InstallDate
     Write-Host "Server name:" $Wmi.BizTalkServer.Name
     Write-Host "SSO Server:" $Wmi.BizTalkGroup.SSOServerName
     Write-Host "BizTalk Admin group:" $Wmi.BizTalkGroup.BizTalkAdministratorGroup
@@ -75,10 +73,84 @@ function ServerDetails ($Wmi)
     $Wmi.Products | where-object { $_.Name -like "*BizTalk*" } | select-object Name -Unique | Sort-Object Name | Select-Object -expand Name
 }
 
+function HostInstances($Wmi)
+{
+    Write-Host "`nHost Instance Information ("$Wmi.HostInstances.Count")" -fore DarkGray
+    
+    foreach ($hostInstance in $Wmi.HostInstances) {
+        switch ($hostInstance.servicestate) {
+            1 { $hostInstanceState = "Stopped" }
+            2 { $hostInstanceState = "Start pending" }
+            3 { $hostInstanceState = "Stop pending" }
+            4 { $hostInstanceState = "Running" }
+            5 { $hostInstanceState = "Continue pending" }
+            6 { $hostInstanceState = "Pause pending" }
+            7 { $hostInstanceState = "Paused" }
+            8 { $hostInstanceState = "Unknown" }
+        }
+        switch ($hostInstance.HostType) {
+            1 { $hostInstanceType = "In-process" }
+            2 { $hostInstanceType = "Isolated" }
+        }
+        if ($hostInstanceState -eq "Running") {
+            Write-Host $hostInstance.hostname "($hostInstanceType)" "- "  -NoNewline
+            Write-Host $hostInstanceState -fore green
+        }
+        elseif ($hostInstanceState -eq "Stopped") {
+                if ($hostInstance.IsDisabled -eq $true ) {
+                    Write-Host $hostInstance.hostname "($hostInstanceType)" "- " -NoNewline
+                    Write-Host $hostInstanceState "(Disabled)" -fore red
+                }
+                else {
+                    Write-Host $hostInstance.hostname "($hostInstanceType)" "- " -NoNewline
+                    Write-Host $hostInstanceState -fore Red
+                }
+        }
+        else {
+            if ($hostInstanceType -eq "In-process") {
+                Write-Host $hostInstance.hostname "($hostInstanceType)" "- " -NoNewline
+                Write-Host $hostInstanceState "(Disabled:$($hostInstance.IsDisabled))" -fore DarkYellow
+            }
+            else {
+                Write-Host $hostInstance.hostname "($hostInstanceType)"
+            }
+        }
+    }
+    Write-Host "`nTracking Host(s)" -Fore DarkGray
+    $Wmi.TrackingHost.Name
+}
+
+function Applications($Wmi)
+{
+    # Get BizTalk Application Information
+    $applications = $Wmi.BizTalkOM.Applications
+
+    # Display BizTalk Application Information
+    Write-Host "`nBizTalk Applications ("$applications.Count")" -fore DarkGray
+
+    Foreach ($application in $applications) 
+    {
+        if ($application.Status -eq "Started") {
+            Write-Host $application.Name "- " -NoNewline
+            Write-Host $application.Status -fore Green
+        }
+        elseif ($application.Status -eq "Stopped") {
+            Write-Host $application.Name "- " -NoNewline
+            Write-Host $application.Status -fore Red
+        }
+        else {
+            Write-Host $application.Name "- " -NoNewline
+            Write-Host $application.Status -fore DarkYellow
+        }
+    }
+}
+
 function ProcessConfiguration ($configurationType, $Wmi)
 {
     switch ($configurationType) {
         "ServerDetails" { ServerDetails($Wmi)  }
+        "HostInstances" { HostInstances($Wmi)  }
+        "Applications" { Applications($Wmi)  }
         Default { Write-Host "Invalid configuration type: $configurationType" -fore Red}
     } 
 }
@@ -98,7 +170,7 @@ function ReadConfigurationData ($HostConfiguration)
 
     foreach($config in $HostConfiguration.Configurations)
     {
-        ProcessConfiguration($config);
+        ProcessConfiguration $config $WmiInterface #Syntax looks weird, but this is how it's done.
     }
 }
 

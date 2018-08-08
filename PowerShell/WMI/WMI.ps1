d94a70d555cb0a736988dbe63c06fab124f37600
Milo Wical <Milo.Wical@compare.com>
2018-08-08 11:00:15 -0400

    More increments.

--- PowerShell/WMI/WMI.ps1
+++ PowerShell/WMI/WMI.ps1
@@ -22,21 +22,23 @@ function LoadHostWmiProviders($HostName)
         [void] [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.BizTalk.ExplorerOM")
         $BizTalkDBInstance = $BizTalkGroup.MgmtDbServerName
         $BizTalkDB = $BizTalkGroup.MgmtDbName
-        $BizTalkOM = New-Object Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer
-        $BizTalkOM.ConnectionString = "SERVER=$BizTalkDBInstance;DATABASE=$BizTalkDB;Integrated Security=SSPI"
 
-        $BizTalkWmiAccess = New-Object PSObject
-        $BizTalkWmiAccess | Add-Member -type NoteProperty -Name 'BizTalkGroup' -Value $BizTalkGroup
-        $BizTalkWmiAccess | Add-Member -type NoteProperty -Name 'BizTalkMsgBoxDb' -Value $BizTalkMsgBoxDb
-        $BizTalkWmiAccess | Add-Member -type NoteProperty -Name 'BizTalkServer' -Value $BizTalkServer
-        $BizTalkWmiAccess | Add-Member -type NoteProperty -Name 'BizTalkREG' -Value $BizTalkREG
-        $BizTalkWmiAccess | Add-Member -type NoteProperty -Name 'HostInstances' -Value $HostInstances
-        $BizTalkWmiAccess | Add-Member -type NoteProperty -Name 'TrackingHost' -Value $TrackingHost
-        $BizTalkWmiAccess | Add-Member -type NoteProperty -Name 'BizTalkDBInstance' -Value $BizTalkDBInstance
-        $BizTalkWmiAccess | Add-Member -type NoteProperty -Name 'BizTalkDB' -Value $BizTalkDB
-        $BizTalkWmiAccess | Add-Member -type NoteProperty -Name 'BizTalkOM' -Value $BizTalkOM
+        #$BizTalkOM = New-Object Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer
+        #$BizTalkOM.ConnectionString = "SERVER=$BizTalkDBInstance;DATABASE=$BizTalkDB;Integrated Security=SSPI"
+        
+        $Products = Get-WmiObject win32_product -ErrorAction Stop -ComputerName $HostName
 
-        Write-Host "${HostName}: BizTalk WMI providers loaded successfully." -fore Green
+        $BizTalkWmiProvider = New-Object PSObject
+        $BizTalkWmiProvider | Add-Member -type NoteProperty -Name 'BizTalkGroup' -Value $BizTalkGroup
+        $BizTalkWmiProvider | Add-Member -type NoteProperty -Name 'BizTalkMsgBoxDb' -Value $BizTalkMsgBoxDb
+        $BizTalkWmiProvider | Add-Member -type NoteProperty -Name 'BizTalkServer' -Value $BizTalkServer
+        $BizTalkWmiProvider | Add-Member -type NoteProperty -Name 'BizTalkREG' -Value $BizTalkREG
+        $BizTalkWmiProvider | Add-Member -type NoteProperty -Name 'HostInstances' -Value $HostInstances
+        $BizTalkWmiProvider | Add-Member -type NoteProperty -Name 'TrackingHost' -Value $TrackingHost
+        $BizTalkWmiProvider | Add-Member -type NoteProperty -Name 'BizTalkDBInstance' -Value $BizTalkDBInstance
+        $BizTalkWmiProvider | Add-Member -type NoteProperty -Name 'BizTalkDB' -Value $BizTalkDB
+        $BizTalkWmiProvider | Add-Member -type NoteProperty -Name 'BizTalkOM' -Value $BizTalkOM
+        $BizTalkWmiProvider | Add-Member -type NoteProperty -Name 'Products' -Value $Products
 
         return $BizTalkWmiProvider;
     }
@@ -49,22 +51,50 @@ function LoadHostWmiProviders($HostName)
     }
 }
 
+function ServerDetails ($Wmi)
+{
+    Write-Host "Server Details: " -fore Blue
+
+    Write-Host "`nBizTalk Information" -fore Green
+    #Write-Host $BiztalkREG.ProductName "("$BiztalkREG.ProductEdition"Edition )"
+    #Write-Host "Product Version:" $BiztalkREG.ProductVersion
+    #Write-Host "Installation Path:" $BiztalkREG.InstallPath
+    #Write-Host "Installation Date:" $BiztalkREG.InstallDate
+    Write-Host "Server name:" $Wmi.BizTalkServer.Name
+    Write-Host "SSO Server:" $Wmi.BizTalkGroup.SSOServerName
+    Write-Host "BizTalk Admin group:" $Wmi.BizTalkGroup.BizTalkAdministratorGroup
+    Write-Host "BizTalk Operators group:" $Wmi.BizTalkGroup.BizTalkOperatorGroup
+    Write-Host "BizTalk Group Name:" $Wmi.BizTalkGroup.Name
+    Write-Host "Cache Refresh Interval:" $Wmi.BizTalkGroup.ConfigurationCacheRefreshInterval
+
+    switch ($Wmi.BizTalkGroup.GlobalTrackingOption) {
+        0 { Write-Host "Global Tracking: Off" }
+        1 { Write-Host "Global Tracking: On" }
+    }
+    Write-Host "`nInstalled BizTalk Software" -Fore DarkGray
+    $Wmi.Products | where-object { $_.Name -like "*BizTalk*" } | select-object Name -Unique | Sort-Object Name | Select-Object -expand Name
+}
+
 function ProcessConfiguration ($configurationType, $Wmi)
 {
     switch ($configurationType) {
         "ServerDetails" { ServerDetails($Wmi)  }
-        Default { Write-Host "Invalid configuration: $configurationType" -fore Red}
+        Default { Write-Host "Invalid configuration type: $configurationType" -fore Red}
     } 
 }
 
 function ReadConfigurationData ($HostConfiguration) 
 {
-    $WmiInterface = LoadHostWmiProviders($HostConfiguration.Name);
+    $HostName = $HostConfiguration.Name;
 
-    If($WmiInterface -eq $null)
+    $WmiInterface = LoadHostWmiProviders($HostName);
+
+    If($null -eq $WmiInterface)
     {
         return;
     }
+
+    Write-Host "${HostName}: BizTalk WMI providers loaded successfully." -fore Green
 
     foreach($config in $HostConfiguration.Configurations)
     {
